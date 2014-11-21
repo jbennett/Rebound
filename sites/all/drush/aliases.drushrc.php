1f65c0b8b9ef75de800eae3e871d53901b1f4956
Chris Morris <cmorris@ccistudios.com>
2014-11-21 14:10:22 -0500

    started the theme

---
+++ sites/all/drush/aliases.drushrc.php
@@ -0,0 +1,13 @@
+<?php
+/*
+$aliases['dev'] = array(
+	'uri'=> 'dev.example.com',
+	'root' => '/home/username/subdomains/dev/public_html',
+	'remote-host'=> 'host.cciserver2.com',
+	'remote-user'=> 'username',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+	'ssh-options'=> '-p 37241'
+);
+*/
