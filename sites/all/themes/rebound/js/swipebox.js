929ab657408b013833c4c231ee16a44eb3943003
Chris Morris <cmorris@ccistudios.com>
2015-02-03 13:33:14 -0500

    Open the gallery popup from the photo galleries page
    close #44

--- sites/all/themes/rebound/js/swipebox.js
+++ sites/all/themes/rebound/js/swipebox.js
@@ -1,5 +1,17 @@
 (function($){    
     $(function(){
         $('.swipebox, .view-program .views-field-field-photos a').swipebox();
+        
+        $(".view-photo-galleries .view-content a").click(function(){
+            var photos = [];
+            var $items = $(this).parents(".views-row").find("[data-href]");
+            for (var i=0; i<$items.length; i++)
+            {
+                photos.push({href: $items.eq(i).data("href")});
+            }
+            $.swipebox(photos);
+            
+            return false;
+        });
     });
 })(jQuery);
